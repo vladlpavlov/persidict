a37c5d57c9cab9ca5bc865645b621da4b315824e
Vlad Pavlov <vlpavlov@ieee.org>
2023-08-22 14:19:25 -0700

    DOC: Add docstrings to safe_str_tuple.py

--- persidict/safe_str_tuple.py
+++ persidict/safe_str_tuple.py
@@ -1,3 +1,6 @@
+"""SafeStrTuple: an immutable flat tuple of non-emtpy URL/filename-safe strings.
+"""
+
 import string
 from collections.abc import Sequence, Mapping, Hashable
 from typing import Tuple, Any
@@ -18,12 +21,20 @@ def _is_sequence_not_mapping(obj:Any)->bool:
         return False
 
 class SafeStrTuple(Sequence, Hashable):
-    """A sequence of non-emtpy URL/filename-safe strings.
+    """An immutable sequence of non-emtpy URL/filename-safe strings.
     """
 
     the_strings: Tuple[str, ...]
 
     def __init__(self, *args):
+        """Create a SafeStrTuple from a sequence/tree of strings.
+
+        The constructor accepts a sequence (list, tuple, etc.) of objects,
+        each of which can be a string or a nested sequence of
+        objects with similar structure. The input tree of strings is flattened.
+        Each string must be non-empty and contain
+        only URL/filename-safe characters.
+        """
         assert len(args) > 0
         candidate = []
         for a in args:
@@ -41,40 +52,51 @@ class SafeStrTuple(Sequence, Hashable):
         self.the_strings = tuple(candidate)
 
     def __getitem__(self, key):
+        """Return a string at position key."""
         return self.the_strings[key]
 
     def __len__(self):
+        """Return the number of strings in the tuple."""
         return len(self.the_strings)
 
     def __hash__(self):
+        """Return a hash of the tuple."""
         return hash(self.the_strings)
 
     @classmethod
     def allowed_characters(cls):
+        """Return a set of allowed characters."""
         return SAFE_CHARS_SET
 
     def __repr__(self):
+        """Return repr(self)."""
         return f"SafeStrSequence({self.the_strings})"
 
 
     def __eq__(self, other):
+        """Return self == other."""
         assert isinstance(other, SafeStrTuple)
         return self.the_strings == other.the_strings
 
 
     def __add__(self, other):
+        """Return self + other."""
         other = SafeStrTuple(other)
         return SafeStrTuple(*(self.the_strings + other.the_strings))
 
     def __radd__(self, other):
+        """Return other + self."""
         other = SafeStrTuple(other)
         return SafeStrTuple(*(other.the_strings + self.the_strings))
 
     def __iter__(self):
+        """Return iter(self)."""
         return iter(self.the_strings)
 
     def __contains__(self, item):
+        """Return item in self."""
         return item in self.the_strings
 
     def __reversed__(self):
+        """Return a reversed SafeStrTuple."""
         return SafeStrTuple(*reversed(self.the_strings))
