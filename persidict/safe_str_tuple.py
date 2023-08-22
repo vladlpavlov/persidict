dc3144b15089119fb96120c1490a5c5a9dca7dd6
Vlad Pavlov <vlpavlov@ieee.org>
2023-08-22 14:41:01 -0700

    CLN: Rename SafeStrTuple.s_chain to SafeStrTuple.flat_str_chain

--- persidict/safe_str_tuple.py
+++ persidict/safe_str_tuple.py
@@ -24,7 +24,7 @@ class SafeStrTuple(Sequence, Hashable):
     """An immutable sequence of non-emtpy URL/filename-safe strings.
     """
 
-    s_chain: Tuple[str, ...]
+    flat_str_chain: Tuple[str, ...]
 
     def __init__(self, *args):
         """Create a SafeStrTuple from a sequence/tree of strings.
@@ -36,32 +36,32 @@ class SafeStrTuple(Sequence, Hashable):
         only URL/filename-safe characters.
         """
         assert len(args) > 0
-        candidate = []
+        candidate_str_chain = []
         for a in args:
             if isinstance(a, SafeStrTuple):
-                candidate.extend(a.s_chain)
+                candidate_str_chain.extend(a.flat_str_chain)
             elif isinstance(a, str):
                 assert len(a) > 0
                 assert len(set(a) - SAFE_CHARS_SET) == 0
-                candidate.append(a)
+                candidate_str_chain.append(a)
             elif _is_sequence_not_mapping(a):
                 if len(a) > 0:
-                    candidate.extend(SafeStrTuple(*a).s_chain)
+                    candidate_str_chain.extend(SafeStrTuple(*a).flat_str_chain)
             else:
                 assert False, f"Invalid argument type: {type(a)}"
-        self.s_chain = tuple(candidate)
+        self.flat_str_chain = tuple(candidate_str_chain)
 
     def __getitem__(self, key):
         """Return a string at position key."""
-        return self.s_chain[key]
+        return self.flat_str_chain[key]
 
     def __len__(self):
         """Return the number of strings in the tuple."""
-        return len(self.s_chain)
+        return len(self.flat_str_chain)
 
     def __hash__(self):
         """Return a hash of the tuple."""
-        return hash(self.s_chain)
+        return hash(self.flat_str_chain)
 
     @classmethod
     def allowed_characters(cls):
@@ -70,33 +70,33 @@ class SafeStrTuple(Sequence, Hashable):
 
     def __repr__(self):
         """Return repr(self)."""
-        return f"SafeStrSequence({self.s_chain})"
+        return f"SafeStrSequence({self.flat_str_chain})"
 
 
     def __eq__(self, other):
         """Return self == other."""
         assert isinstance(other, SafeStrTuple)
-        return self.s_chain == other.s_chain
+        return self.flat_str_chain == other.flat_str_chain
 
 
     def __add__(self, other):
         """Return self + other."""
         other = SafeStrTuple(other)
-        return SafeStrTuple(*(self.s_chain + other.s_chain))
+        return SafeStrTuple(*(self.flat_str_chain + other.flat_str_chain))
 
     def __radd__(self, other):
         """Return other + self."""
         other = SafeStrTuple(other)
-        return SafeStrTuple(*(other.s_chain + self.s_chain))
+        return SafeStrTuple(*(other.flat_str_chain + self.flat_str_chain))
 
     def __iter__(self):
         """Return iter(self)."""
-        return iter(self.s_chain)
+        return iter(self.flat_str_chain)
 
     def __contains__(self, item):
         """Return item in self."""
-        return item in self.s_chain
+        return item in self.flat_str_chain
 
     def __reversed__(self):
         """Return a reversed SafeStrTuple."""
-        return SafeStrTuple(*reversed(self.s_chain))
+        return SafeStrTuple(*reversed(self.flat_str_chain))
