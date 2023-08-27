758dc936536303f443488f4d22dfd09f09c4026f
Vlad Pavlov <vlpavlov@ieee.org>
2023-08-27 08:46:08 -0700

    TST: Add unit-test for SAFE_CHARS_SET

--- persidict/tests/test_safe_chars.py
+++ persidict/tests/test_safe_chars.py
@@ -1,5 +1,10 @@
-from persidict.safe_chars import replace_unsafe_chars
+from persidict.safe_chars import replace_unsafe_chars, SAFE_CHARS_SET
 
 def test_replace_unsafe_chars():
     assert replace_unsafe_chars("a@b*c", "_") == "a_b_c"
     assert replace_unsafe_chars("X Y&Z", "") == "XYZ"
+
+def test_safe_chars_set():
+    assert isinstance(SAFE_CHARS_SET, set)
+    assert len(SAFE_CHARS_SET) >= 52
+    assert len(SAFE_CHARS_SET & set("\t\n\r\\/")) == 0
