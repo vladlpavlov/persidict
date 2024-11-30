644569c3ed70d0f75eef6c3d9b68fce2401318c0
Vlad Pavlov <vlpavlov@ieee.org>
2024-11-30 12:10:46 -0800

    BLD: elevate version to 0.8.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="persidict"
-    ,version="0.8.7"
+    ,version="0.8.8"
     ,author="Vlad (Volodymyr) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description= "Simple persistent key-value store for Python. " 
