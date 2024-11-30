ac4fbc24f4c16cbdb765a1bee40adcca520dc42f
Vlad Pavlov <vlpavlov@ieee.org>
2024-11-30 11:59:59 -0800

    TST: refactor test_concurrency()

--- persidict/tests/test_concurrency_filedirdict.py
+++ persidict/tests/test_concurrency_filedirdict.py
@@ -5,16 +5,16 @@ from persidict import FileDirDict
 def many_operations(dir_name:str, process_n:int):
     d = FileDirDict(dir_name)
     d["a"] = random.random()
-    try:
-        for i in range(50):
+    for i in range(50):
+        try:
             time.sleep(random.random())
             for j in range(50):
                 if random.random() < 0.5:
                     d["a"] = random.random()
                 else:
                     _ = d["a"]
-    except:
-        d[f"error_in_process_{i}"] = True
+        except:
+            d[f"error_in_process_{i}"] = True
 
 def test_concurrency(tmpdir):
     dir_name = str(tmpdir)
